948be38d0c7052161f1526a26b0a0e1013c99836
Koketso-dax <lanchaster.k@gmail.com>
2021-08-28 18:22:43 +0200

    added user post component

--- src/Forum.js
+++ src/Forum.js
@@ -1,8 +1,11 @@
 import React from "react";
 import "./Forum.css";
+import Card from "./components/postCard";
 
 function Forum() {
    return <div className="forum">
+
+     <Card/>
      
   </div>;
 }

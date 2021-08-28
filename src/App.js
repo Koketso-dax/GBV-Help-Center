7554cefc7c2ad5b0442d829f9f215646e6c15884
brian <attomg@gmail.com>
2021-08-28 14:05:12 +0200

    login page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+  const user = null;
+
   return (
     <div className="App">
-
+      {!user ? (
+        <Login />
+      ) : (
+        <>
+          <Forum />
+        </>
+      )}
     </div>
   );
 }
